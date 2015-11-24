45609253861fd14ac8c9c8495fbc32ae93382d51
Layla Mandella <laylamandella@gmail.com>
2015-11-23 18:16:27 -0800

    tweet to a follower

--- bot.js
+++ bot.js
@@ -2,3 +2,23 @@ var Twit = require('twit');
 var config = require('./config');
 var T = new Twit(config);
 
+// get the list of followers
+T.get('followers/list', { screen_name: '[twitter_handle_here]' },  function (err, data, response) {
+	if ( err ) {
+		console.error('error getting the list of followers because of: ' + err);
+	}
+	// tweet to the first one on your list (your latest follower)
+	console.log(data.users[0].screen_name);
+	tweet( data.users[0].screen_name );
+})
+
+
+
+function tweet( twitterHandle ) {
+	var followersHandle = '@' + twitterHandle;
+	var messageToThem = 'hey there!';
+	var tweetToSend = followersHandle + ' ' + messageToThem;
+	T.post('statuses/update', { status: tweetToSend }, function(err, data, response) {
+		console.log(data);
+	});
+}
