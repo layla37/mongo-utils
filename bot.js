1d8aa445e5f599d50ece7183b287ee6546d50fc7
Layla Mandella <laylamandella@gmail.com>
2015-11-27 20:00:37 -0800

    get the 20 most recent followers

--- bot.js
+++ bot.js
@@ -4,14 +4,14 @@ var T = new Twit(config);
 
 // get the list of followers
 T.get('followers/list', { screen_name: '[twitter_handle_here]' },  function (err, data, response) {
+	var listOfFollowers = [];
 	if ( err ) {
 		console.error('error getting the list of followers because of: ' + err);
 	}
-	// tweet to the first one on your list (your latest follower)
-	console.log(data.users[0].screen_name);
-	tweet( data.users[0].screen_name );
+	for (var i = 0; i < data.users.length; i++ ) {
+		listOfFollowers.push(data.users[i].screen_name);
+	}
 })
-
 
 
 function tweet( twitterHandle ) {
