b4d6f3f760c382c4f8b9675b587cf72b73cec86e
Layla Mandella <laylamandella@gmail.com>
2015-11-27 20:02:36 -0800

    listen for direct messages

--- bot.js
+++ bot.js
@@ -11,9 +11,9 @@ T.get('followers/list', { screen_name: '[twitter_handle_here]' },  function (err
 	for (var i = 0; i < data.users.length; i++ ) {
 		listOfFollowers.push(data.users[i].screen_name);
 	}
-})
+});
 
-
+// tweet to someone
 function tweet( twitterHandle ) {
 	var followersHandle = '@' + twitterHandle;
 	var messageToThem = 'hey there!';
@@ -22,3 +22,12 @@ function tweet( twitterHandle ) {
 		console.log(data);
 	});
 }
+
+var stream = T.stream('user');
+// listen for a direct message
+stream.on('direct_message', function (data) {
+	var follower = data.direct_message.sender_screen_name;
+	var dm = data.direct_message.text;
+	console.log(follower);
+	console.log(dm);
+});
